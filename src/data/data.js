2dad6189156507a27b3fae42054164b415dfc408
zildanpandarusm <zildanmarginata26@gmail.com>
2025-10-17 02:45:30 +0700

    update

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+export let users = [
+  {
+    id: '88d1d10a-a63c-42d6-9d82-68b02b6d3284',
+    name: 'Siti Aminah',
+    username: 'sitiaminah',
+    email: 'siti@example.com',
+    address: 'Jl. Merpati No. 12, Jakarta',
+    createdAt: new Date('2025-10-16T19:01:22.957Z'),
+    updatedAt: new Date('2025-10-16T19:01:22.957Z'),
+  },
+];
--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,32 +1,23 @@
-import { readUsers, writeUsers } from '../data/data-handler.js';
+import { users } from '../data/data.js';
 
-export const getAllUsersRepo = () => readUsers();
+export const getAllUsersRepo = () => users;
 
-export const getUserByIdRepo = (id) => {
-  const users = readUsers();
-  return users.find((u) => u.id === id) || null;
-};
+export const getUserByIdRepo = (id) => users.find((u) => u.id === id) || null;
 
 export const saveUserRepo = (user) => {
-  const users = readUsers();
   users.push(user);
-  writeUsers(users);
 };
 
 export const updateUserRepo = (user) => {
-  const users = readUsers();
   const index = users.findIndex((u) => u.id === user.id);
   if (index !== -1) {
     users[index] = user;
-    writeUsers(users);
   }
 };
 
 export const deleteUserRepo = (id) => {
-  const users = readUsers();
   const index = users.findIndex((u) => u.id === id);
   if (index !== -1) {
     users.splice(index, 1);
-    writeUsers(users);
   }
 };
